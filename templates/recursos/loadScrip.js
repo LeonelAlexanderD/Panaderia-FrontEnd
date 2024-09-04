b9238f25f49c335931d7440983d404d306dae7ac
Leonel Alexander Diaz <chaffs25@gmail.com>
2024-09-04 20:21:47 -0300

    fix: corregido los directorios que generaban problemas

--- templates/recursos/loadScrip.js
+++ templates/recursos/loadScrip.js
@@ -1,14 +1,13 @@
-contenidoHeader
 
 function contentLoader() {
-    fetch('/templates/resources/header.html')
+    fetch('/templates/recursos/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('contenidoHeader').innerHTML = data;
         })
         .catch(error => console.error('Error al cargar el header:', error));
 
-    fetch('/templates/resources/footer.html')
+    fetch('/templates/recursos/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('contenidoFooter').innerHTML = data;
